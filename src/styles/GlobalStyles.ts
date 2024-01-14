7a3f0b743ae418711160c5dc7a87b292256c5ded
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-01-14 17:21:13 +0100

    #1 add static Score component

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'Tektur';
+  font-style: normal;
+  font-weight: 700;
+  font-stretch: 100%;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/tektur/v3/XoH62YHtS7q969kXCjzlV0aSkS_o8OacszucvA.woff2) format('woff2');
+}
+
 * {
     padding: 0;
     margin: 0;
