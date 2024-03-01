30639ef8b1a0a7e355d5a73afc682b4f7f31094b
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-03-01 20:00:11 +0100

    #23 filled rows state, falling speed based on filled rows

--- src/hooks/useFallingBlock.ts
+++ src/hooks/useFallingBlock.ts
@@ -1,17 +1,13 @@
 import { useEffect, useRef, useCallback } from "react";
-import { handleBlockSettle } from "$utils/handleBlockSettle";
 import {
   BinaryMatrix,
-  BlockVectors,
   Vector,
   RenderableBlockDefinition,
 } from "$types/typeCollection";
 import getRenderableBlock from "$utils/getRandomBlock";
 import { moveBlockByOne, renderableBlockList } from "$utils/block/block";
-import { SPAWN_LOCATION } from "$config/initialSettings";
 
 interface FallingBlockProps {
-  blockVectors: BlockVectors;
   setActiveBlock: React.Dispatch<
     React.SetStateAction<RenderableBlockDefinition>
   >;
@@ -20,18 +16,19 @@ interface FallingBlockProps {
   setStaticBlocksMatrix: React.Dispatch<React.SetStateAction<BinaryMatrix>>;
   canMoveDown: boolean;
   fallInterval: number;
+  handleEndFall: () => void;
 }
 
 const MIN_INTERVAL = 0;
 
 export default function useFallingBlock({
-  blockVectors,
   setActiveBlock,
   setHookLocation,
   staticBlocksMatrix,
   setStaticBlocksMatrix,
   canMoveDown,
   fallInterval,
+  handleEndFall,
 }: FallingBlockProps) {
   const passedIntervalTime = useRef(0);
   const intervalStartTimestamp = useRef(0);
@@ -43,13 +40,6 @@ export default function useFallingBlock({
       setActiveBlock(block);
     },
     [setActiveBlock]
-  );
-
-  const resetHookLocation = useCallback(
-    function resetHookLocation() {
-      setHookLocation(() => SPAWN_LOCATION);
-    },
-    [setHookLocation]
   );
 
   const handleFall = useCallback(
@@ -65,21 +55,19 @@ export default function useFallingBlock({
         if (canMoveDown) {
           moveBlockByOne(setHookLocation, "down");
         } else {
-          handleBlockSettle({ blockVectors, setStaticBlocksMatrix });
-          resetHookLocation();
+          handleEndFall();
           spawnBlock();
         }
       }, Math.max(MIN_INTERVAL, fallInterval - passedIntervalTime.current));
     },
     [
-      blockVectors,
       canMoveDown,
-      resetHookLocation,
       setHookLocation,
       setStaticBlocksMatrix,
       spawnBlock,
       staticBlocksMatrix,
       fallInterval,
+      handleEndFall,
     ]
   );
 
