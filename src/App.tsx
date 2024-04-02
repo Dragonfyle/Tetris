f60572024560b9da03cf9f3ae3453d48604b80cb
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-04-02 22:08:52 +0200

    #63 fix buggy firebase login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useSyncExternalStore } from "react";
 import { GlobalStyles } from "$styles/GlobalStyles";
 import MainCanvas from "$components/MainCanvas/MainCanvas";
 import { calculateScore } from "$utils/score";
 import { useAppSelector } from "$utils/typedReduxHooks";
 import { selectRowsFilled } from "$store/rowsFilledSlice";
 import { useAppDispatch } from "$utils/typedReduxHooks";
-import { updateScores } from "$store/highScoresSlice";
+import { selectHighScores, updateScores } from "$store/highScoresSlice";
 import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import { getAuth, signInAnonymously, Auth } from "firebase/auth";
 
 function App() {
+  const { highScores } = useAppSelector((state) => selectHighScores(state));
   const dispatch = useAppDispatch();
 
   const { numRowsFilled } = useAppSelector((state) => selectRowsFilled(state));
   const score = calculateScore(numRowsFilled);
 
   const db = useRef(getFirestore());
+  const auth = useRef(getAuth());
+
+  useEffect(() => {
+    signIn(auth.current);
+  });
+
+  useSyncExternalStore(keepScoresUpToDate, getSnapshot);
 
   function keepScoresUpToDate() {
     const scoresRef = doc(db.current, "highScores", "top10");
@@ -39,13 +47,9 @@ function App() {
     });
   }
 
-  useEffect(() => {
-    const auth = getAuth();
-    const unsub = keepScoresUpToDate();
-    signIn(auth);
-
-    return () => unsub();
-  }, []);
+  function getSnapshot() {
+    return highScores;
+  }
 
   return (
     <>
