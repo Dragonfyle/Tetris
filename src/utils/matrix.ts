60081a1c7136d5f37be2a6ff743b760188d8c131
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-04-27 20:48:17 +0200

    code cleanup

--- src/utils/matrix.ts
+++ src/utils/matrix.ts
@@ -1,25 +1,30 @@
 import { ColorCodeRow, ColorCodeMatrix } from "$types/typeCollection";
 
+interface Acc {
+  currentRow: number;
+  matrix: ColorCodeMatrix;
+}
+
 function createRow(length: number) {
   return Array(length).fill(0);
 }
 
 function makeMatrixFromArray(array: ColorCodeRow, rowLength: number) {
-  return array.reduce(
+  return array.reduce<Acc>(
     (acc, el, idx) => {
       if (idx % rowLength === 0 && idx) {
         acc.currentRow += 1;
       }
 
       if (!acc.matrix[acc.currentRow]) {
-        acc.matrix[acc.currentRow] = [] as ColorCodeRow;
+        acc.matrix[acc.currentRow] = [];
       }
 
       acc.matrix[acc.currentRow].push(el);
 
       return acc;
     },
-    { currentRow: 0, matrix: [] as ColorCodeMatrix }
+    { currentRow: 0, matrix: [] }
   ).matrix;
 }
 
--- src/utils/score.ts
+++ src/utils/score.ts
@@ -1,5 +1,7 @@
+import { SINGLE_ROW_SCORE, TETRIS_ADD_SCORE } from "$config/initialSettings";
+
 function calculateScore(numRowsFilled: number, num4rows: number) {
-  return numRowsFilled * 100 + num4rows * 400;
+  return numRowsFilled * SINGLE_ROW_SCORE + num4rows * TETRIS_ADD_SCORE;
 }
 
 export { calculateScore };
